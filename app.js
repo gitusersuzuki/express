9b886d196c9a0ddc026800ef31161469185c864d
Yuki Kudo <2101125453hs@cyber-u.ac.jp>
2025-05-17 14:10:22 +0900

    test web api

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');    // ADD_20250510
+var notesRouter = require('./routes/notes');    // ADD_20250517
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);   // ADD_20250510
+app.use('/notes', notesRouter);   // ADD_20250517
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
