986faef7662b6d3a26d44ac9ed07183ac885bca0
Yuki Kudo <2101125453hs@cyber-u.ac.jp>
2025-06-01 08:55:14 +0900

    add yes_no

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');    // ADD_20250510
 //var notesRouter = require('./routes/notes');    // ADD_20250517  // ※notes.jsでURLを養生しているため一時的にコメントアウトしとく
 var catRouter   = require('./routes/cat');      // ADD_20250531
+var yes_noRouter = require('./routes/yes_no');      // ADD_20250601
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use('/users', usersRouter);
 app.use('/hello', helloRouter);   // ADD_20250510
 //app.use('/notes', notesRouter);   // ADD_20250517  // ※notes.jsでURLを養生しているため一時的にコメントアウトしとく
 app.use('/cat', catRouter);       // ADD_20250531
+app.use('/yes_no', yes_noRouter); // ADD_20250601
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
